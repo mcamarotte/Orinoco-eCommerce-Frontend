bbb0fe5766544d18115e770d51c28ff5b0997403
mcamarotte <76053716+mcamarotte@users.noreply.github.com>
2021-04-06 11:49:44 +0100

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
 const basketButton = document.getElementById('basket-button');
 const reportName = document.getElementsByClassName('card-title');
-const reportPrice = document.getElementsByClassName('prod-price');
+const reportDesc = document.getElementsByClassName('prod-price');
 const reportLink = document.getElementsByClassName('card-link');
 const reportImg = document.getElementsByClassName('card-image');
 const URL = 'http://localhost:3000/api/cameras';
@@ -19,17 +19,17 @@ let apiRequest = new XMLHttpRequest();
 apiRequest.onreadystatechange = () => {
   if(apiRequest.readyState === 4) {    
 	if(apiRequest.status === 200 || apiRequest.status === 201) {  		
-		const response = JSON.parse(apiRequest.response);			
+		const response = JSON.parse(apiRequest.response);
 		for (var i = 0; i < response.length; i++) {			
 			reportName[i].textContent = response[i].name;		
-			reportPrice[i].textContent = response[i].price;
+			reportDesc[i].textContent = response[i].description;
 			reportLink[i].href = "product.html?id=" + response[i]._id;
 			reportImg[i].src = response[i].imageUrl;
 		}
 	} else {	
 		serverError.innerHTML = "There is a problem with the server's response";
 		reportName.textContent = 'Name Not Found!';
-		reportPrice.textContent = 'Description Not Found!';
+		reportDesc.textContent = 'Description Not Found!';
 		reportLink.href = 'product.html';
 		reportImg.src = 'images/vcam_1.jpg';
 	}	
