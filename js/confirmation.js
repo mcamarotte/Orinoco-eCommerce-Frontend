bdd23ec8e5b5e32efdf6b8bce0c9c08c0a4af441
mcamarotte <76053716+mcamarotte@users.noreply.github.com>
2021-04-15 12:27:09 +0100

    Update confirmation.js

--- js/confirmation.js
+++ js/confirmation.js
@@ -4,6 +4,7 @@ const confPrice = document.getElementById('conf-price');
 const confTitle = document.getElementById('confirmation');
 const confCard = document.getElementById('conf-card');
 
+//take query parameter id to help display product
 function queryString(obj) {  
     const result = [];
     let match;
@@ -13,6 +14,8 @@ function queryString(obj) {
     }
     return result;
 }
+
+//display price correctly by dividing by 100 displaying it with two decimal points 
 function financial(y) {
 	let price= y/100;
 	return parseFloat(price).toFixed(2);	
@@ -21,14 +24,18 @@ function financial(y) {
 const orderNumber = queryString("id")[0];
 const total = queryString("total")[0];
 
+//if order number is not displayed then display error page
 if (orderNumber === "") {
 
+	//if id is not detected then display error page that no item is detected 
 	confTitle.textContent = 'You have not Purchased an item';
 	confCard.remove();
 } else {
 
+	//once order is completed local storage clear all its items
 	localStorage.clear();
 
+	//if request is successful then display confirmation of purchased items	
 	confName.textContent = "Your order number is: " + orderNumber; 
 	confPrice.textContent = "Price: $" + financial(total);
 }
